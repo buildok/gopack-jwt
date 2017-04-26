cfa948ec365480f6115e2170f3c741b8c7f944ee
buildok <buildok@mail.ru>
2017-04-26 14:29:44 +0300

    scsc

--- jwt.go
+++ jwt.go
@@ -1,7 +1,37 @@
 package jwt
 
-import ()
+import (
+	"encoding/json"
+)
 
 func Hello() string {
 	return "hello world!"
+}
+
+type header struct {
+	alg string
+	typ string
+}
+
+type JWT struct {
+	key string
+	hdr string
+}
+
+func New(key string) *JWT {
+	jwt = new(JWT)
+	jwt.key = key
+
+	hdr := header{
+		alg: "HS256",
+		typ: "JWT",
+	}
+	res, _ := json.Marshal(hdr)
+	jwt.hdr = string(res)
+
+	return jwt
+}
+
+func (j *JWT) Encode() string {
+	return "Encode"
 }
